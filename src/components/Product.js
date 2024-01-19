ce30fdbe6b2dded5a54dc928f8fea463c49377d5
MuhammadFerySyahputra <Ferys2343@gmail.com>
2024-01-19 22:23:31 +0700

    second commit

---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+const Product = () => {
+    return (
+      <div>
+        <ul>
+          <li>produk 1 - 899</li>
+          <li>produk 2 - 982</li>
+          <li>produk 3 - 322</li>
+          <li>produk 4 - 763</li>
+          <li>produk 5 - 389</li>
+        </ul>
+      </div>
+    );
+}
+
+export default Product;
